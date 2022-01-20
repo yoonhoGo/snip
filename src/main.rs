0250aa11acc7a88e7a62ab737184aeccdd1dad58
yoonhoGo <yoonho.go@erion.kr>
2022-01-20 23:01:36 +0900

    feat: add snippet structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+struct Snippet {
+    name: String,
+    prefix: Vec<String>,
+    body: Vec<String>,
+    description: String,
+    scope: Option<String>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
